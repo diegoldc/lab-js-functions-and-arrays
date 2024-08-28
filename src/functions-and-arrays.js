f2ddc21581e4bd4bd17ddd6060bded4b383b83b8
Diego Lázaro <dlazarodecastro@gmail.com>
2024-08-28 23:24:58 +0200

    lab functions commienzo

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,42 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else if (num2 > num1) {
+        return num2;
+    } else {
+        return num1;
+    }
+}
 
 
 
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = ["mystery", "brother", "aviator", "crocodile", "patatagorda", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(array) {
+    
+    let longestWord = words[0];
+
+    for (let i = 0; i < array.length; i++) {
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];
+    }
+
+    if ( words[i].length > longestWord.length) {
+        console.log(`${longestWord[i].length} is longer than ${words.length}`);
+      }
+}
+
+if (words.length === 0) {
+    return null;
+  }
+
+return longestWord;
+}
+
+console.log(findLongestWord(words));
 
 
 
